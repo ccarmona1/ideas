7b55c9442dbbe24e834fd22fea1637f553cf58d2
Crisman <crisman999@outlook.com>
2025-06-11 18:51:12 -0500

    feat: Add Prettier configuration and mark additional TypeScript topics as completed

--- javascript/coding/typescript/creacion/src/index.ts
+++ javascript/coding/typescript/creacion/src/index.ts
@@ -7,3 +7,16 @@ export async function holiwi(params: string) {
 }
 
 await holiwi('holiwi');
+
+type ElementType<T> = T extends (infer U)[] ? U : T;
+
+type A = ElementType<bigint[]>; // bigint
+type B = ElementType<string>; // string
+
+type T<T> = T extends number ? string : boolean;
+type R = T<1 | true>;
+
+type ReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+
+type D = ReturnType<() => number>; // number
+type E = ReturnType<(x: string) => boolean>; // boolean

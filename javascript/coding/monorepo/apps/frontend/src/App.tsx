d1e14fe8b02ed7292f201ff0903d957e1c6a68f6
Crisman <crisman999@outlook.com>
2025-06-21 16:16:15 -0500

    fix: update header text to 'Tester app' and remove unused font imports

--- javascript/coding/monorepo/apps/frontend/src/App.tsx
+++ javascript/coding/monorepo/apps/frontend/src/App.tsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import './App.css';
 import { Body } from './features/core/components/body/Body';
 
--- javascript/coding/monorepo/apps/frontend/src/features/core/components/header/Header.tsx
+++ javascript/coding/monorepo/apps/frontend/src/features/core/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export const Header: React.FunctionComponent = () => {
-  return <>My Header</>;
+  return <>Tester app</>;
 };
 export default Header;

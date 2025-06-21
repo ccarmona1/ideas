3b71606c4719d0eb4e2f0ff36fff53cb2da0a5d9
Crisman <crisman999@outlook.com>
2025-06-21 16:14:01 -0500

    Refactor code structure for improved readability and maintainability

--- javascript/coding/monorepo/apps/frontend/src/features/core/components/body/Body.tsx
+++ javascript/coding/monorepo/apps/frontend/src/features/core/components/body/Body.tsx
@@ -1,16 +1,10 @@
-import { Grid } from '@mui/material';
 import Header from '../header/Header';
 
 export const Body: React.FunctionComponent = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid size={12}>
-        <Header></Header>
-      </Grid>
-      <Grid size={4}>Hola 4</Grid>
-      <Grid size={4}>Hola 4</Grid>
-      <Grid size={8}>Hola 8</Grid>
-    </Grid>
+    <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr' }}>
+      <Header></Header>
+    </div>
   );
 };
 

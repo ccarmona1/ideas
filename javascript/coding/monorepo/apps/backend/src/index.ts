1133ec6b23c77ce2932139691a4ca409989a20d1
Crisman <crisman999@outlook.com>
2025-06-22 18:34:09 -0500

    feat: add environment variable checks and JSON files for automatic exams in JavaScript and TypeScript

--- javascript/coding/monorepo/apps/backend/src/index.ts
+++ javascript/coding/monorepo/apps/backend/src/index.ts
@@ -2,21 +2,33 @@ import { Octokit } from '@octokit/rest';
 import { generateForm } from './generateForm.js';
 
 async function init() {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  if (!GITHUB_TOKEN) {
+    throw new Error('GITHUB_TOKEN must be set in environment variables');
+  }
+
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
   const GITHUB_OWNER = process.env.GITHUB_OWNER;
   const GITHUB_REPO = process.env.GITHUB_REPO;
-  const path = 'questions.json';
+  if (!GITHUB_OWNER || !GITHUB_REPO) {
+    throw new Error(
+      'GITHUB_OWNER and GITHUB_REPO must be set in environment variables'
+    );
+  }
+  const path = '/javascript/examenes/automaticos';
 
-  const {
-    data: { sha },
-  } = await octokit.repos.getContent({
+  const response = await octokit.repos.getContent({
     owner: GITHUB_OWNER,
     repo: GITHUB_REPO,
-    path,
+    path: path,
   });
 
-  await generateForm();
+  const courses = response.data;
+
+  console.log(response);
+
+  //await generateForm();
 }
 
 await init();

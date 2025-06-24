56303a4bd054cc4498f48b8c65410c60c62d25a6
Crisman <crisman999@outlook.com>
2025-06-23 22:01:24 -0500

    feat: update Playwright config for video settings and add new test for dragging explanation

--- javascript/coding/monorepo/packages/e2e/playwright.config.ts
+++ javascript/coding/monorepo/packages/e2e/playwright.config.ts
@@ -30,7 +30,11 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    video: 'on-first-retry',
+    video: 'on',
+
+    launchOptions: {
+      slowMo: process.env.CI ? 0 : 2000,
+    },
   },
 
   /* Configure projects for major browsers */
